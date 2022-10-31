998d90ed2167f890f818a56a7937c4463c7d0ec7
Camel <camel_exvl@outlook.com>
2022-10-31 21:06:10 +0800

    fix message

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     DD_BOT_SECRET = os.getenv("DD_BOT_SECRET")
     res = json.loads(res)
     dingpush = dingPush.dingpush(
-        "青年大学习签到结果", "青年大学习签到成功：\n" + "状态码：" + res["status"] + "\n课程ID: " +
+        "青年大学习签到结果", "青年大学习签到成功：\n" + "状态码：" + str(res["status"]) + "\n课程ID: " +
         current_course + "\n签到学号: " + res["result"]["cardNo"] + "\n签到时间: " +
         res["result"]["lastUpdTime"], "", DD_BOT_TOKEN, DD_BOT_SECRET)
     dingpush.SelectAndPush()
