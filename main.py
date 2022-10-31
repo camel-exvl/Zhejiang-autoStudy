7f803923615765c5503b50db30fd1d9f6d6f7b38
Camel <camel_exvl@outlook.com>
2022-10-31 20:00:29 +0800

    create main

---
+++ main.py
@@ -0,0 +1,69 @@
+import json
+import os
+import time
+
+import requests
+
+
+def getAccessToken(session, openid):
+    time_stamp = str(int(time.time()))  #获取时间戳
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/login/we-chat/callback?callback=https%3A%2F%2Fqczj.h5yunban.com%2Fqczj-youth-learning%2Findex.php&scope=snsapi_userinfo&appid=wx56b888a1409a2920&openid=" + openid + "&nickname=ZhangSan&headimg=&time=" + time_stamp + "&source=common&sign=&t=" + time_stamp
+    res = session.get(url)
+    access_token = res.text[45:81]
+    print("获取到AccessToken:", access_token)
+    return access_token
+
+
+def getCurrentCourse(session, access_token):
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/common-api/course/current?accessToken=" + access_token
+    res = session.get(url)
+    res_json = json.loads(res.text)
+    if (res_json["status"] == 200):
+        print("获取到最新课程代号:", res_json["result"]["id"])
+        return res_json["result"]["id"]
+    else:
+        raise Exception("获取最新课程失败！")
+
+
+def getJoin(session, access_token, current_course, nid, cardNo):
+    data = {
+        "course": current_course,  # 大学习期次的代码，如C0046，本脚本已经帮你获取啦
+        "subOrg": None,
+        "nid": nid,  # 团组织编号，形如N003************
+        "cardNo": cardNo  # 打卡昵称
+    }
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/join?accessToken=" + access_token
+    res = session.post(url, json=data)
+    print("签到结果:", res.text)
+    res_json = json.loads(res.text)
+    if (res_json["status"] == 200):
+        print("似乎签到成功了")
+        return True
+    else:
+        raise Exception("签到失败！")
+
+
+if __name__ == '__main__':
+    nid = os.getenv("nid")
+    cardNo = os.getenv("cardNo")
+    openid = os.getenv("openid")
+
+    session = requests.session()
+    session.headers = {
+        'User-Agent':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4'
+    }
+    try:
+        # 获取token
+        time.sleep(5)
+        access_token = getAccessToken(session, openid)
+
+        # 获取最新的章节
+        time.sleep(5)
+        current_course = getCurrentCourse(session, access_token)
+
+        # 签到
+        time.sleep(5)
+        getJoin(session, access_token, current_course, nid, cardNo)
+    except Exception as e:
+        print(e)
